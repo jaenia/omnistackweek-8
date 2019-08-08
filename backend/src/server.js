ff35dd89fdf7353e0bbfdea8b49c008dae52408a
Jaênia <jaeniaps@gmail.com>
2019-08-08 18:27:25 -0300

    Adding cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
@@ -8,6 +10,7 @@ mongoose.connect('mongodb+srv://jaenia:omnistack@cluster0-5nta5.mongodb.net/omni
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
